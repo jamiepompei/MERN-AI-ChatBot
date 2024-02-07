e35100058af1926025c0eddbdfd2d3a4170d38bd
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-02-07 17:55:29 -0500

    user login

--- backend/dist/controllers/user-controller.js
+++ backend/dist/controllers/user-controller.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 export const getAllUsers = async (req, res, next) => {
     try {
         const users = await User.find();
@@ -13,9 +13,33 @@ export const getAllUsers = async (req, res, next) => {
         });
     }
 };
+export const userLogin = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(401).send("User not registered.");
+        }
+        const isPasswordCorrect = await compare(password, existingUser.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).send("Incorrect password");
+        }
+        return res.status(200).json({ message: "OK", id: existingUser._id.toString });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({
+            message: "ERROR",
+            cause: error.message
+        });
+    }
+};
 export const userSignup = async (req, res, next) => {
     try {
         const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser)
+            return res.status(401).send("User already registered");
         const hashedPassword = await hash(password, 10);
         const user = new User({ name, email, password: hashedPassword });
         await user.save();
