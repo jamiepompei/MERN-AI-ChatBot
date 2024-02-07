3a1533d0f885157f83a5d3c88e18703edf488d8e
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-02-07 14:37:57 -0500

    set up connection to mongodb

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
-const app = express();
-// middleware
-app.use(express.json());
-// connection and listener
-app.listen(5000, () => console.log("AI ChatBot Server Open"));
+import app from "./app.js";
+import { connectToDatabase } from "./database/connection.js";
+// connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("AI ChatBot Server Open & Connected to Database! "));
+}).catch(err => console.log(err));
 //# sourceMappingURL=index.js.map
