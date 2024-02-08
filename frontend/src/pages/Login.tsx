819bf817510d73b8c1afd06396132f9803605a4b
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-02-08 15:29:19 -0500

    login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,8 +9,6 @@ const Login = () => {
         const formData = new FormData(e.currentTarget);
         const email = formData.get("email");
         const password = formData.get("password");
-        console.log(email);
-        console.log(password);
     }
     return <Box width={'100%'} height={'100%'} display="flex" flex={1}>
         <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
