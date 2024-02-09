2fa726cacb3a34779b74fa5cddf6de1089dd99dd
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-02-09 12:05:24 -0500

    chat and chat item, temp listof chat messages

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,36 +1,150 @@
 import React from 'react';
-import  { Box, Avatar, Typography, Button } from "@mui/material"
+import  { Box, Avatar, Typography, Button, IconButton } from "@mui/material"
+import { IoMdSend } from "react-icons/io"
 import { red } from "@mui/material/colors"
 import { useAuth } from '../context/AuthContext';
+
+const chat_messages = [
+    {"role": "user", "content": "Hello, can you tell me the weather forecast for tomorrow?"},
+    {"role": "assistant", "content": "Sure! I can help you with that. Please provide me with your location."},
+    {"role": "user", "content": "I am in new York City"},
+    {"role": "assistant", "content": "Great! Give me a moment to fetch that information for you."},
+    {"role": "assistant", "content": "The weather forecast for New York City tomorrow is: Sunny with a chance of clouds."},
+]
 
 const Chat = () => {
     const auth = useAuth();
     return (
-            <Box sx={{ display: "flex", flex: 1, width: "100%", height: "100%", mt: 3, gap: 3, }}
-            >
-            <Box sx={{ display: { md: "flex", xs: "none,", sm: "none" }, flex: 0.2, flexDirection: "column," }}></Box>
-            <Box sx={{ display: "flex", width: "100%", height:"60vh", bgcolor: "rgb(17,29,39)", borderRadius: 5,
-        flexDirection: "column",
-        mx: 3 }}
+        <Box
+        sx={{
+          display: "flex",
+          flex: 1,
+          width: "100%",
+          height: "100%",
+          mt: 3,
+          gap: 3,
+        }}
+      >
+        <Box
+          sx={{
+            display: { md: "flex", xs: "none", sm: "none" },
+            flex: 0.2,
+            flexDirection: "column",
+          }}
         >
-            <Avatar sx={{ mx: "auto", my: 2, bgcolor: "white", color: "black", fontWeight: 700, }}>
-                {auth?.user?.name[0]}
-                {auth?.user?.name.split(" ")[1][0]}
+          <Box
+            sx={{
+              display: "flex",
+              width: "100%",
+              height: "60vh",
+              bgcolor: "rgb(17,29,39)",
+              borderRadius: 5,
+              flexDirection: "column",
+              mx: 3,
+            }}
+          >
+            <Avatar
+              sx={{
+                mx: "auto",
+                my: 2,
+                bgcolor: "white",
+                color: "black",
+                fontWeight: 700,
+              }}
+            >
+              {auth?.user?.name[0]}
+              {auth?.user?.name.split(" ")[1][0]}
             </Avatar>
-            <Typography sx={{ mx: 'auto', fontFamily: "work-sans",  }}>You are talking to a ChatBOT</Typography>
-            <Typography sx={{ mx: 'auto', fontFamily: "work-sans", my: 2, p: 3,  }}>You can ask questions related to general knowledge, business, education, etc but avoid sharing personal information.
+            <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+              You are talking to a ChatBOT.
             </Typography>
-            <Button sx={{ width: "200px", my: "auto", color: "white", fontWeight: 700, borderRadius: 3, mx: "auto", bgcolor: red[300], ":hover": {
-                bgcolor: red.A400,
-            }  }}>Clear Conversation
+            <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+              You can ask some questions related to general knowledge, business, 
+              education, etc, but avoid sharing personal information.
+            </Typography>
+            <Button
+        
+              sx={{
+                width: "200px",
+                my: "auto",
+                color: "white",
+                fontWeight: "700",
+                borderRadius: 3,
+                mx: "auto",
+                bgcolor: red[300],
+                ":hover": {
+                  bgcolor: red.A400,
+                },
+              }}
+            >
+              Clear Conversation
             </Button>
+          </Box>
         </Box>
-        <Box sx={{ display: "flex", flex: { md: 0.8, xs: 1, sm: 1 } }}>
-            <Typography sx={{ textAlign: "center", fontSize: "40px", color: "white", mb: 2 }}>
-                Model - GPT 3.5 Turbo
-            </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flex: { md: 0.8, xs: 1, sm: 1 },
+            flexDirection: "column",
+            px: 3,
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: "40px",
+              color: "white",
+              mb: 2,
+              mx: "auto",
+              fontWeight: "600",
+            }}
+          >
+            Model - GPT 3.5 Turbo
+          </Typography>
+          <Box
+            sx={{
+              width: "100%",
+              height: "60vh",
+              borderRadius: 3,
+              mx: "auto",
+              display: "flex",
+              flexDirection: "column",
+              overflow: "scroll",
+              overflowX: "hidden",
+              overflowY: "auto",
+              scrollBehavior: "smooth",
+            }}
+          >
+            {chat_messages.map((chat) => <div>{ chat.content }</div>)}
+          </Box>
+          <div
+            style={{
+              width: "100%",
+              borderRadius: 8,
+              backgroundColor: "rgb(17,27,39)",
+              display: "flex",
+              margin: "auto",
+            }}
+          >
+            {" "}
+            <input
+            
+              type="text"
+              style={{
+                width: "100%",
+                backgroundColor: "transparent",
+                padding: "30px",
+                border: "none",
+                outline: "none",
+                color: "white",
+                fontSize: "20px",
+              }}
+            />
+            <IconButton sx={{ color: "white", mx: 1, padding: 3 }}>
+              <IoMdSend />
+            </IconButton>
+          </div>
         </Box>
-        </Box>
+      </Box>
     );
 };
 
