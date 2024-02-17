e04e6540799033f9d64e2df7cbf84aa27d1e0002
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-02-17 12:24:45 -0500

    user dto and file renaming

--- backend/dist/services/user-service.js
+++ backend/dist/services/user-service.js
@@ -1,2 +1,42 @@
-export {};
+import User from "../models/user.js";
+export class UserService {
+    async getAllUsers() {
+        try {
+            return await User.find();
+        }
+        catch (error) {
+            throw new Error("Error fetching users " + error.message);
+        }
+    }
+    ;
+    async getUserByEmail(email) {
+        try {
+            return await User.findOne({ email });
+        }
+        catch (error) {
+            throw new Error("Error fetching user by email " + error.message);
+        }
+    }
+    ;
+    async getUserById(id) {
+        try {
+            return await User.findById(id);
+        }
+        catch (error) {
+            throw new Error("Error fetching user by id " + error.message);
+        }
+    }
+    ;
+    async saveUser(name, email, hashedPassword) {
+        try {
+            const userToSave = new User({ name, email, hashedPassword });
+            return await userToSave.save();
+        }
+        catch (error) {
+            throw new Error("Error saving user: " + error.message);
+        }
+        ;
+    }
+    ;
+}
 //# sourceMappingURL=user-service.js.map
