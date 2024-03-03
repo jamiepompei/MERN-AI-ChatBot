acefddb4fcc09d7a1ce79df19ff8054db32e345c
jamiepompei <jamie.lynn.pompei@gmail.com>
2024-03-02 18:39:29 -0600

    add constants for the api roles, slight refactor on api config and instance

--- backend/src/services/chat-service.ts
+++ backend/src/services/chat-service.ts
@@ -1,9 +1,10 @@
 import { ChatCompletionRequestMessage, OpenAIApi } from "openai";
 import { AuthenticationService } from "./authentication-service.js";
-import { configureOpenAI } from "../config/openai-config.js";
+import { configureOpenAI, getOpenAIAPIInstance } from "../config/openai-config.js";
 import { UserDTO, ChatDTO } from "../models/User.js";
 import { Types } from "mongoose";
 import { UserService } from "./user-service.js";
+import { ASSISTANT_ROLE, GPT_MODEL, USER_ROLE } from "../utils/constants.js";
 
 
 const userService = new UserService();
@@ -16,16 +17,13 @@ export class ChatService {
             const user: UserDTO = await userService.getUserById(userId);
             authService.verifyUserByTokenId(user);
             const chats = user.chats.map(({ role, content }) => ({ role, content })) as ChatCompletionRequestMessage[];
-            chats.push({ content: message, role: "user" });
+            chats.push({ content: message, role: USER_ROLE });
             user.chats.push({
-               content: message, role: "user",
+               content: message, role: USER_ROLE,
             });
 
-            const config = configureOpenAI();
-            const openAI = new OpenAIApi(config);
-
-            const chatResponse = await openAI.createChatCompletion({ model: "gpt-3.5-turbo", messages: chats });
-            const chat: ChatDTO = { role: "assistant", content: chatResponse.data.choices[0].message.content };
+            const chatResponse = await getOpenAIAPIInstance().createChatCompletion({ model: GPT_MODEL, messages: chats });
+            const chat: ChatDTO = { role: ASSISTANT_ROLE, content: chatResponse.data.choices[0].message.content };
             user.chats.push(chat);
             await userService.updateUserChats(user);
             return user.chats;
--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,2 +1,5 @@
 export const COOKIE_NAME = "auth_token";
 export const TOKEN_EXPIRATION = "7d";
+export const ASSISTANT_ROLE = "assistant";
+export const USER_ROLE = "user";
+export const GPT_MODEL = "gpt-3.5-turbo";
